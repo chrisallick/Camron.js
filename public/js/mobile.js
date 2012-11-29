c601bb88c008fe6af47af71187518699b82c2395
Chris Allick <chrisallick@gmail.com>
2012-11-29 11:14:30 -0800

    added gestures

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,14 +1,19 @@
 var camronphone;
 $(document).ready(function(){
-	// document.addEventListener('touchmove',function(e) {
-	// 	e.preventDefault();
-	// });
+    document.body.addEventListener('touchmove',function(e){
+        if(!$(e.target).hasClass(".scrollable")) {
+            e.preventDefault();
+        }
+    });
 
-	camronphone = new CamronPhone( window, $("fileinput") );
+    camronphone = new CamronPhone( window, $("fileinput") );
 
-	$("#file").change(function(){
-		camronphone.handleFile( this.files[0] );
-	});
+    $("#file").change(function(){
+        camronphone.handleFile( this.files[0] );
+    });
+
+    var index = 0;
+    var $sw = $('#images');
 
     $.ajax({
         type: "GET",
@@ -20,25 +25,45 @@ $(document).ready(function(){
     }).done(function(resp){
         if( resp && resp.images && resp.images.length > 0 ) {
             for( var i = 0, len = resp.images.length; i < len; i++ ) {
-            	var image = $("<div>").addClass("image");
-            	var img = new Image();
-            	$(img).hide();
-            	img.onload = function() {
-            		var w = this.width;
-            		var h = this.height;
-            		$(this).css({
-            			width: w,
-            			height: h,
-            			top: 640/2 - h/2,
-            			left: 640/2 - w/2
-            		}).fadeIn();
-            	}
-            	img.src = resp.images[i];
-            	$(image).css({
-            		left: i*640
-            	}).html(img);
-            	$("#images").append(image);
+                var image = $("<div>").addClass("image");
+                var img = new Image();
+                $(img).hide();
+                img.onload = function() {
+                    var w = this.width;
+                    var h = this.height;
+                    $(this).css({
+                        width: w,
+                        height: h,
+                        top: 640/2 - h/2,
+                        left: 640/2 - w/2
+                    }).fadeIn();
+                }
+                img.src = resp.images[i];
+                $(image).css({
+                    left: i*640
+                }).html(img);
+                $("#images").append(image);
             }
+
+            $sw.on('swipe', function (event) {
+                event.preventDefault();
+
+                if( ["left","right"].indexOf(event.direction) != -1 ) {
+                    if( event.direction == "right" && index > 0 ) {
+                        index--;
+                        $(".image").animate({
+                            left: "+=640"
+                        });
+                        console.log("right", index);
+                    } else if( event.direction == "left" && index <= $(".image").length-1 ) {
+                        index++;
+                        $(".image").animate({
+                            left: "-=640"
+                        });
+                        console.log("left", index);
+                    }
+                }
+            });
         }
     });
 });
